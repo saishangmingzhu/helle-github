11115590a51a51897905810f13128b587b80d867
saishangmingzhu <1094768388@qq.com>
2015-09-09 12:05:12 +0800

    first

---
+++ src/com/zuohao/HelloGithub.java
@@ -0,0 +1,10 @@
+package com.zuohao;
+
+/**
+ * Created by zuohao on 15/9/9.
+ */
+public class HelloGithub {
+    public static void main(String[] args){
+        System.out.println("Hello,GitHub!");
+    }
+}

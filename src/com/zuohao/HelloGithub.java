2d9a8f1032bb68d999dafce1567df837505c6c1c
saishangmingzhu <1094768388@qq.com>
2015-09-09 16:43:46 +0800

    again

--- src/com/zuohao/HelloGithub.java
+++ src/com/zuohao/HelloGithub.java
@@ -6,5 +6,6 @@ package com.zuohao;
 public class HelloGithub {
     public static void main(String[] args){
         System.out.println("Hello,GitHub!");
+        System.out.println("Hello,GitHub!");
     }
 }
